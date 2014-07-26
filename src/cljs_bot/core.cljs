5726852cc2b874bfb7aa926d41af08e4e6f5515a
Shaun Williams <swilliams@pros.com>
2014-07-26 14:00:28 -0500

    add bouncing lights

--- src/cljs_bot/core.cljs
+++ src/cljs_bot/core.cljs
@@ -12,23 +12,49 @@
 
 (def board (Board.))
 
-(defn go-go-blink!
-  [led]
+(defn rand-int2
+  [a b]
+  (+ a (rand-int (inc (- b a)))))
+
+(defn go-go-bounce!
+  [leds rate bounces]
   (go
-    (loop []
-      (.on led)
-      (<! (timeout 100))
-      (.off led)
-      (<! (timeout 1000))
-      (recur)))
-  )
+    (dotimes [_ bounces]
+
+      ; do a bounce
+      (doseq [i [0 1 2 3 4 3 2 1]]
+
+        ; turn on/off appropriate lights
+        (doseq [[j led] (map-indexed vector leds)]
+          (if (= i j)
+            (.on led)
+            (.off led)))
+
+        (if (= i 4)
+          (<! (timeout (* 2 rate))))
+
+        ; pause
+        (<! (timeout rate))))
+
+    ; turn off all lights
+    (doseq [led leds] (.off led))))
 
 (defn on-ready
   []
-  (let [led (Led. 13)]
-    (.. board -repl (inject #js {:led led}))
-    (go-go-blink! led)
-  ))
+  (let [leds [(Led. 8)
+              (Led. 9)
+              (Led. 10)
+              (Led. 11)
+              (Led. 12)]]
+
+    (go
+      (loop []
+        (<! (go-go-bounce! leds
+                 (rand-int2 25 100)
+                 (rand-int2 1 3)))
+        (recur)))
+    
+    ))
 
 (defn -main [& args]
   (.on board "ready" on-ready))
